46a1c371562717902cb1cca9aa1e38b6cd7af55b
Winstone <you@example.com>
2024-01-11 22:57:43 +0300

    my another commit

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def Home(request):
-    return render(request, 'home.html')
+    context={
+        "name": "Marjesty Owino",
+        "number": 1123
+    }
+    return render(request, 'home.html', context)
 
 def News(request):
-    return render(request, 'News.html')
+    context={
+        "list": ["python", "django", "kotlin", "C++", "Javascript", "Java"],
+        "mynum": 50
+    }
+    return render(request, 'News.html', context)
 def Contact(request):
-    return render(request, 'Contact.html')
+    return render(request, 'Contact.html',)
